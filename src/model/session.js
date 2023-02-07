66c9d9a4abdcedaeef8db11035b03e21c8eed698
Karol <105356599+Kr33L@users.noreply.github.com>
2023-02-07 14:55:02 +0000

    Update session.js

--- src/model/session.js
+++ src/model/session.js
@@ -17,7 +17,7 @@ function createSession(user_id) {
 const select_session = db.prepare(/*sql*/ `
     SELECT id, user_id, expires_at
     FROM sessions WHERE id = ?
-    `);
+`);
 
 function getSession(sid) {
   return select_session.get(sid);
