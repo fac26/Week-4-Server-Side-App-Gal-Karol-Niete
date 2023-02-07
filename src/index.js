16a0720ba7adb440cb6e77621464274445ad48c7
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-02-07 17:53:02 +0000

    broken home()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const server = require('./server.js');
+const server = require("./server");
 const port = process.env.PORT || 8080;
 const log = console.log(`Listening on http://localhost:${port}`);
 
---
+++ src/routes/home.js
@@ -0,0 +1,11 @@
+const { layout, home } = require("../templates/template");
+
+function get(request, response) {
+	console.log(response);
+	const title = "Home | I :heart: Food!";
+	const content = home();
+	const html = layout(title, content);
+	response.send(html);
+}
+
+module.exports = { get };
