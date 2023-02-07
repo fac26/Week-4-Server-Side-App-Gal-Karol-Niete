4643a41845e0531a8427bd274a7ff733aa23a25a
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-02-07 14:25:50 +0000

    add model/session.js

---
+++ src/model/session.js
@@ -0,0 +1,35 @@
+const crypto = require('node:crypto');
+const db = require('../database/db.js');
+
+// create session
+const insert_session = db.prepare(/*sql*/ `
+    INSERT INTO sessions (id, user_id, expires_at) 
+    VALUES ($id, $user_id, DATE('now', '+7 days'))
+`);
+
+function createSession(user_id) {
+  const id = crypto.randomBytes(18).toString('base64');
+  insert_session.run({ id, user_id });
+  return id;
+}
+
+// select session
+const select_session = db.prepare(/*sql*/ `
+    SELECT id, user_id, expires_at
+    FROM sessions WHERE id = ?
+    `);
+
+function getSession(sid) {
+  return select_session.get(sid);
+}
+
+// delete session
+const delete_session = db.prepare(/*sql*/ `
+    DELETE FROM sessions WHERE id = ?
+`);
+
+function removeSession(sid) {
+  return delete_session.run(sid);
+}
+
+module.exports = { createSession, getSession, removeSession };
