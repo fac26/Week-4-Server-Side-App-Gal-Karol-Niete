2cf7e8a35601b3add4484d9cfde594afb2716d2e
Karol <105356599+Kr33L@users.noreply.github.com>
2023-02-08 12:09:16 +0000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const bodyParser = express.urlencoded({ extended: false });
 const { sessions } = require('./templates/helper');
 
-const server = express();
-const cookies = cookieParser(process.env.COOKIE_SECRET); //process.env.COOKIE_SECRET
+const cookies = cookieParser(process.env.COOKIE_SECRET);
+const bodyParser = express.urlencoded({ extended: false });
 
-const signUp = require('./routes/sign-up');
-
-server.use(cookies); //pass cookieParser to all reoutes with req object
-server.use(sessions);
-
-server.get('/sign-up', signUp.get);
-server.post('/sign-up', bodyParser, signUp.post);
 server.use(cookies);
+server.use(sessions);
 
 const home = require('./routes/home');
 const login = require('./routes/login');
+const signUp = require('./routes/sign-up');
+
+const server = express();
 
 server.get('/', home.get);
+server.get('/sign-up', signUp.get);
 server.get('/log-in', login.get);
+server.post('/sign-up', bodyParser, signUp.post);
+server.post('/log-in', bodyParser, login.post);
 
 module.exports = server;
