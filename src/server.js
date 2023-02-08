91525d2a5a0dbe01b6ac57b8b1af611ce87dfb38
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-02-08 11:58:52 +0000

    add signed cookie  middleware, sign-up post function

--- src/server.js
+++ src/server.js
@@ -2,18 +2,10 @@ const express = require('express');
 const { getSession } = require('./model/session');
 const cookieParser = require('cookie-parser');
 const bodyParser = express.urlencoded({ extended: false });
+const { sessions } = require('./templates/helper');
 
 const server = express();
 const cookies = cookieParser(process.env.COOKIE_SECRET); //process.env.COOKIE_SECRET
-
-function sessions(req, res, next) {
-  const sid = req.signedCookies.sid;
-  const session = getSession(sid);
-  if (session) {
-    req.session = session;
-  }
-  next();
-}
 
 const signUp = require('./routes/sign-up');
 
@@ -21,6 +13,7 @@ server.use(cookies); //pass cookieParser to all reoutes with req object
 server.use(sessions);
 
 server.get('/sign-up', signUp.get);
+server.post('/sign-up', bodyParser, signUp.post);
 server.use(cookies);
 
 const home = require('./routes/home');
