e6e03e89b6ba8b08538634f5f50ad4cbe24a8dc1
space doge <99536044+d0g3bl2@users.noreply.github.com>
2023-02-08 11:32:45 +0000

    add post function

--- src/routes/sign-up.js
+++ src/routes/sign-up.js
@@ -1,4 +1,6 @@
 const { layout, signUpForm } = require('../templates/template');
+const createUser = require('./model/user');
+const createSession = require('./model/session');
 
 function get(request, response) {
 	const title = 'Sign up | I :heart: food!';
@@ -12,9 +14,21 @@ function get(request, response) {
 function post (request, response) {
     const { name, email, password } = request.body;
 
-    if (!username || !password) {
+    if (!name || !email || !password) {
       response.status(400).send('<h1>Login Failed.</h1>');
     }
+
+    bcrypt.hash(password, 12).then((hash) => {
+			const user = createUser(username, hash);
+			const session_id = createSession(user.id);
+			response.cookie('sid', session_id, {
+				signed: true,
+				httpOnly: true,
+				maxAge: 1000 * 60 * 60 * 24 * 7,
+				sameSite: 'lax',
+			});
+			response.redirect(`/`);
+		});
 }
 
-module.exports = { get };
+module.exports = { get, post };
--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const { getSession } = require('./model/session');
 const cookieParser = require('cookie-parser');
 // const bodyParser = express.urlencoded({ extended: false });
 
 const server = express();
 const cookies = cookieParser(process.env.COOKIE_SECRET); //process.env.COOKIE_SECRET
 
+function sessions(req, res, next) {
+  const sid = req.signedCookies.sid;
+  const session = getSession(sid);
+  if (session) {
+    req.session = session;
+  }
+  next();
+}
+
 const signUp = require('./routes/sign-up');
 
 server.use(cookies); //pass cookieParser to all reoutes with req object
+server.use(sessions);
 
 server.get('/sign-up', signUp.get);
 
